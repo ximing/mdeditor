eee62ed878903f536dae399ac90491e03d86cd61
ximing <807754634@qq.com>
2019-03-25 22:50:00 +0800

    feat: 升级依赖

--- example/index.js
+++ example/index.js
@@ -2,16 +2,15 @@
  * Created by yeanzhi on 17/4/12.
  */
 'use strict';
-import 'babel-polyfill';
-import React, {Component} from "react";
-import rab, {connect, createAction} from 'rabjs/index.js';
-import {Router, Route} from 'rabjs/router';
+import React, { Component } from 'react';
+import rab, { connect, createAction } from 'rabjs/index.js';
+import { Router, Route } from 'rabjs/router';
 import DemoContainer from './DemoContainer';
 const app = rab({
-    debug:true
+    debug: true
 });
 
-app.router(({history}) => {
+app.router(({ history }) => {
     return (
         <Router history={history}>
             <Route path="/demo.html" component={DemoContainer} />
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,12 @@
 'use strict';
-const {
-    resolve
-} = require('path');
+const { resolve } = require('path');
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        'demo': [
+        demo: [
             'react-hot-loader/patch',
             'webpack-dev-server/client?http://127.0.0.1:11001/',
             'webpack/hot/only-dev-server',
@@ -22,9 +20,11 @@ module.exports = {
         publicPath: '/dist/'
     },
     devtool: 'cheap-module-eval-source-map',
-
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
     devServer: {
-        contentBase: [path.join(__dirname,'demo'), path.join(__dirname, 'dist')],
+        contentBase: [path.join(__dirname, 'demo'), path.join(__dirname, 'dist')],
         compress: true,
         port: 11001,
         host: '0.0.0.0',
@@ -52,46 +52,29 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                use:[{
-                    loader: 'babel-loader',
-                    options: {
-                        'presets': [
-                            ['es2015', {
-                                'modules': false
-                            }], 'stage-0', 'react'
-                        ],
-                        'env': {},
-                        'ignore': [
-                            'node_modules/**',
-                            'dist'
-                        ],
-                        'plugins': [
-                            'react-hot-loader/babel',
-                            'transform-decorators-legacy'
-                        ]
+                use: [
+                    {
+                        loader: 'babel-loader'
                     }
-                }],
+                ],
                 exclude: /node_modules/
             },
             {
                 test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'postcss-loader'
-                ]
+                use: ['style-loader', 'css-loader', 'postcss-loader']
             },
             {
                 test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader'
-                ]
-            }, {
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+            },
+            {
                 test: /\.(png|jpg|jpeg|gif|woff|svg|eot|ttf|woff2)$/i,
                 use: ['url-loader']
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
             }
         ]
     },
@@ -103,9 +86,9 @@ module.exports = {
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NamedModulesPlugin(),
         new HtmlWebpackPlugin({
-            title:'test',
-            template:"example/tpl.ejs",
-            inject:false
+            title: 'test',
+            template: 'example/tpl.ejs',
+            inject: false
         })
     ]
 };
